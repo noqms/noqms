25541c07033360ed5463f3053cd0306ee49b8d9f
noqms <55850256+noqms@users.noreply.github.com>
2019-09-30 16:56:14 -0600

    Delete module-info.java

--- module-info.java
+++
@@ -1,9 +0,0 @@
-module com.noqms {
-    requires gson;
-    requires java.sql;
-    
-    opens com.noqms.framework to gson;
-    
-    exports com.noqms.framework to com.noqms;
-    exports com.noqms;
-}
